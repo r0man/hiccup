9006dea040fce3a57380523ca3114cecb5514f45
Roman <roman@burningswell.com>
2012-04-05 21:17:09 +0200

    Moved sources and tests into subdir.

--- project.clj
+++ project.clj
@@ -2,5 +2,7 @@
   :description "A fast library for rendering HTML in Clojure"
   :url "http://github.com/weavejester/hiccup"
   :dependencies [[org.clojure/clojure "1.2.1"]]
+  :source-paths ["src/clj"]
+  :test-paths ["test/clj"]
   :plugins [[codox "0.6.0"]]
   :codox {:exclude [hiccup.compiler]})
