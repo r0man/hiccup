02b0bbd121d88be48c40e82dd7481619482a38ee
Roman <roman@burningswell.com>
2012-04-08 14:49:19 +0200

    Ported tests from def namespace.

--- test/cljs/hiccup/test.cljs
+++ test/cljs/hiccup/test.cljs
@@ -1,5 +1,6 @@
 (ns hiccup.test
   (:require [hiccup.test.core :as core]
+            [hiccup.test.def :as def]
             [hiccup.test.element :as element]
             [hiccup.test.form :as form]
             [hiccup.test.page :as page]
@@ -7,6 +8,7 @@
 
 (defn ^:export run []
   (core/test)
+  (def/test)
   (element/test)
   (form/test)
   (page/test)
